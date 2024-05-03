8683fdb2404003316443db46892002b130cbb9d1
thejitenpatel <jiten.pateljp2001@gmail.com>
2024-05-03 12:27:20 +0530

    Initial Commit

---
+++ SpotifyClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpotifyClone
+//
+//  Created by Jiten Patel on 03/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpotifyClone/SpotifyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyCloneApp.swift
+//  SpotifyClone
+//
+//  Created by Jiten Patel on 03/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
